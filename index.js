f1ea694452eb72ec115cfa2c67b2d6e32d4a9c87
Connor Page <george@connorpage.com>
2024-05-22 10:40:26 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards (list, occasion) {
+    let messages = []
+    for (let i = 0; i < list.length; i++) {
+         messages.push( `Thank you, ${list[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return messages
+}
+
+function countDown (interger) {
+    for (let i = interger; i >= 0; i--) {
+        console.log(i);
+    }
+}
